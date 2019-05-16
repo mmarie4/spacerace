f213928d32d7899482e0785d4a230d25681a03d5
Max MARIE <max.marie@atos.net>
2019-05-16 16:08:42 +0200

    top 10

--- server.js
+++ server.js
@@ -14,24 +14,28 @@ app.use(express.json())
 app.post('/new-score', function(req, res) {
     fs.readFile('leaderboard.json', 'utf8', function(err, data) {
         leaderboard = data != undefined ? JSON.parse(data) : [];
-        // Find smaller score
-        var minScore = undefined;
-        var indexMin = undefined;
-        for (var i = 0; i < leaderboard.length; i++) {
-            if(minScore == undefined) {
-                minScore = leaderboard[i].score;
-            } else {
-                if(leaderboard[i].score < minScore) {
+        if(leaderboard.length >= 10) {
+            // Find smaller score
+            var minScore = undefined;
+            var indexMin = undefined;
+            for (var i = 0; i < leaderboard.length; i++) {
+                if(minScore == undefined) {
                     minScore = leaderboard[i].score;
-                    indexMin = i;
+                } else {
+                    if(leaderboard[i].score < minScore) {
+                        minScore = leaderboard[i].score;
+                        indexMin = i;
+                    }
                 }
             }
-        }
-        if (indexMin != undefined) {
-            if (req.body.score > minScore) {
-                leaderboard.splice(i, 1);
-                leaderboard.push({name: req.body.name, score: req.body.score});
+            if (indexMin != undefined) {
+                if (req.body.score > minScore) {
+                    leaderboard.splice(i, 1);
+                    leaderboard.push({name: req.body.name, score: req.body.score});
+                }
             }
+        } else {
+            leaderboard.push({name: req.body.name, score: req.body.score});
         }
         fs.writeFile('leaderboard.json', JSON.stringify(leaderboard), (err) => {
             if(err) throw err;
