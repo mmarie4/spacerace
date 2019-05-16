7a4c06c4c6dd44f27f5e8babe0ef8e4d1630c72d
Max MARIE <max.marie@atos.net>
2019-05-16 16:01:55 +0200

    keep only 10 best in leaderboard

--- server.js
+++ server.js
@@ -15,12 +15,30 @@ app.post('/new-score', function(req, res) {
     fs.readFile('leaderboard.json', 'utf8', function(err, data) {
         leaderboard = data != undefined ? JSON.parse(data) : [];
         leaderboard.push({name: req.body.name, score: req.body.score});
-        // TODO : keep only top 10 before writing
+        // Find smaller score
+        var minScore = undefined;
+        var indexMin = undefined;
+        for (var i = 0; i < leaderboard.length; i++) {
+            if(minScore == undefined) {
+                minScore = leaderboard[i].score;
+            } else {
+                if(leaderboard[i].score < minScore) {
+                    minScore = leaderboard[i].score;
+                    indexMin = i;
+                }
+            }
+        }
+        if (indexMin != undefined) {
+            if (req.body.score > indexMin) {
+                leaderboard.splice(i, 1);
+                leaderboard.push({name: req.body.name, score: req.body.score});
+            }
+        }
         fs.writeFile('leaderboard.json', JSON.stringify(leaderboard), (err) => {
             if(err) throw err;
         });
+        res.end();
     });
-   res.end();
 });
 
 app.listen(8080);
