905ff6d5073531db541b913c7eb5daa5697038da
max marie <max.marie.4@gmail.com>
2019-11-23 15:53:08 +0800

    Kp updated and latest version of Three.js to fix gltf bug

--- public/js/spaceship.js
+++ public/js/spaceship.js
@@ -1,96 +1,116 @@
 var createShip = function(scene, y, z, xspeed, yspeed) {
-    var loader = new THREE.GLTFLoader();
-    var callback = function(gltf) {
-        ship = gltf.scene;
-        ship.position.y = y;
-        ship.position.z = z;
-        ship.rotation.y = Math.PI / 2;
-        ship.xSpeed = xspeed;
-        ship.ySpeed = yspeed;
-        ship.zSpeed = 0;
-        ship.accel = 0;
-        ship.lastBoost = new Date();
-        ship.hitbox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
-        ship.move = function() {
-            this.hitbox.set(this.position, this.position);
-            if (this.upPressed) {
-                this.position.y += this.ySpeed + this.accel;
-                this.light.position.y += this.ySpeed + this.accel;
-            }
-            if (this.downPressed) {
-                this.position.y -= this.ySpeed + this.accel;
-                this.light.position.y -= this.ySpeed + this.accel;
-            }
-            if (this.rightPressed)  {
-                this.position.x += this.xSpeed + this.accel;
-                this.light.position.x += this.xSpeed + this.accel;
-            }
-            if (this.leftPressed) {
-                this.position.x -= this.xSpeed + this.accel;
-                this.light.position.x -= this.xSpeed + this.accel;
-            }
-            this.position.z -= this.accel;
-            if (this.accel > 0) {
-                this.accel -= DECAY_BOOST;
-            } else {
-                this.accel = 0;
-            }
-            if(this.accel == 0) {
-                this.reactor1.scale.set(5, 5, 5);
-                this.reactor2.scale.set(5, 5, 5);
-                this.reactor3.scale.set(5, 5, 5);
-            }
-            ship.reactor1.position.set(ship.position.x - 0.1, ship.position.y + 3.78, ship.position.z + 10);
-            ship.reactor2.position.set(ship.position.x - 0.65, ship.position.y + 3.35, ship.position.z + 10);
-            ship.reactor3.position.set(ship.position.x + 0.45, ship.position.y + 3.35, ship.position.z + 10);
-            this.hitbox.setFromObject(this);
-        }
-        ship.checkCollisions = function(objects, scene) {
-            objects.forEach(o => {
-                if (o.hitbox.intersectsBox(this.hitbox)) scene.end();
-            });
-        }
-        ship.boost = function() {
-            if (Math.abs(new Date() - this.lastBoost) > BOOST_COOLDOWN_MS) {
-                this.lastBoost = new Date();
-                this.accel = BOOST;
-                document.getElementById("boost-text").setAttribute("style", "opacity: 0.2");
-                this.reactor1.scale.set(8, 8, 8);
-                this.reactor2.scale.set(8, 8, 8);
-                this.reactor3.scale.set(8, 8, 8);
-            }
-        }
-        ship.checkBoost = function() {
-            if (Math.abs(new Date() - this.lastBoost) > BOOST_COOLDOWN_MS) {
-                document.getElementById("boost-text").setAttribute("style", "opacity: 1.0");
-            }
-        },
-        ship.kill = function(scene) {
-            scene.remove(this);
-            scene.remove(this.reactor1);
-            scene.remove(this.reactor2);
-            scene.remove(this.reactor3);
-            scene.remove(this.light);
-        }
-        ship.light = new THREE.PointLight(0xffffff, 0.5, 0, 2);
-        ship.light.position = ship.position;
-        ship.light.position.y += 5;
-        ship.light.position.x += 5;
-        // Sprites : https://ui-ex.com/explore/transparent-circle-light/
-        new THREE.TextureLoader().load("res/reactor.png", function(spriteMap) {
-            var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
-            ship.reactor1 = new THREE.Sprite( spriteMaterial );
-            ship.reactor2 = new THREE.Sprite( spriteMaterial );
-            ship.reactor3 = new THREE.Sprite( spriteMaterial );
-            ship.reactor1.scale.set(5, 5, 5);
-            ship.reactor2.scale.set(5, 5, 5);
-            ship.reactor3.scale.set(5, 5, 5);
-            scene.add( ship.reactor1 );
-            scene.add( ship.reactor2 );
-            scene.add( ship.reactor3 );
-        });
-        scene.add(ship.light);
-        scene.ship = ship;
-    }
-    loader.load('res/joined-spaceship.glb', callback);
-}
+  var loader = new THREE.GLTFLoader();
+  var callback = function(gltf) {
+    console.log("gltf:", gltf);
+    ship = gltf.scene;
+    ship.position.y = y;
+    ship.position.z = z;
+    ship.rotation.y = Math.PI / 2;
+    ship.xSpeed = xspeed;
+    ship.ySpeed = yspeed;
+    ship.zSpeed = 0;
+    ship.accel = 0;
+    ship.lastBoost = new Date();
+    ship.hitbox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+    ship.move = function() {
+      this.hitbox.set(this.position, this.position);
+      if (this.upPressed) {
+        this.position.y += this.ySpeed + this.accel;
+        this.light.position.y += this.ySpeed + this.accel;
+      }
+      if (this.downPressed) {
+        this.position.y -= this.ySpeed + this.accel;
+        this.light.position.y -= this.ySpeed + this.accel;
+      }
+      if (this.rightPressed) {
+        this.position.x += this.xSpeed + this.accel;
+        this.light.position.x += this.xSpeed + this.accel;
+      }
+      if (this.leftPressed) {
+        this.position.x -= this.xSpeed + this.accel;
+        this.light.position.x -= this.xSpeed + this.accel;
+      }
+      this.position.z -= this.accel;
+      if (this.accel > 0) {
+        this.accel -= DECAY_BOOST;
+      } else {
+        this.accel = 0;
+      }
+      if (this.accel == 0) {
+        this.reactor1.scale.set(5, 5, 5);
+        this.reactor2.scale.set(5, 5, 5);
+        this.reactor3.scale.set(5, 5, 5);
+      }
+      ship.reactor1.position.set(
+        ship.position.x - 0.1,
+        ship.position.y + 3.78,
+        ship.position.z + 10
+      );
+      ship.reactor2.position.set(
+        ship.position.x - 0.65,
+        ship.position.y + 3.35,
+        ship.position.z + 10
+      );
+      ship.reactor3.position.set(
+        ship.position.x + 0.45,
+        ship.position.y + 3.35,
+        ship.position.z + 10
+      );
+      this.hitbox.setFromObject(this);
+    };
+    ship.checkCollisions = function(objects, scene) {
+      objects.forEach(o => {
+        if (o.hitbox.intersectsBox(this.hitbox)) scene.end();
+      });
+    };
+    ship.boost = function() {
+      if (Math.abs(new Date() - this.lastBoost) > BOOST_COOLDOWN_MS) {
+        this.lastBoost = new Date();
+        this.accel = BOOST;
+        document
+          .getElementById("boost-text")
+          .setAttribute("style", "opacity: 0.2");
+        this.reactor1.scale.set(8, 8, 8);
+        this.reactor2.scale.set(8, 8, 8);
+        this.reactor3.scale.set(8, 8, 8);
+      }
+    };
+    (ship.checkBoost = function() {
+      if (Math.abs(new Date() - this.lastBoost) > BOOST_COOLDOWN_MS) {
+        document
+          .getElementById("boost-text")
+          .setAttribute("style", "opacity: 1.0");
+      }
+    }),
+      (ship.kill = function(scene) {
+        scene.remove(this);
+        scene.remove(this.reactor1);
+        scene.remove(this.reactor2);
+        scene.remove(this.reactor3);
+        scene.remove(this.light);
+      });
+    ship.light = new THREE.PointLight(0xffffff, 0.5, 0, 2);
+    ship.light.position = ship.position;
+    ship.light.position.y += 5;
+    ship.light.position.x += 5;
+    // Sprites : https://ui-ex.com/explore/transparent-circle-light/
+    new THREE.TextureLoader().load("res/reactor.png", function(spriteMap) {
+      var spriteMaterial = new THREE.SpriteMaterial({
+        map: spriteMap,
+        color: 0xffffff
+      });
+      ship.reactor1 = new THREE.Sprite(spriteMaterial);
+      ship.reactor2 = new THREE.Sprite(spriteMaterial);
+      ship.reactor3 = new THREE.Sprite(spriteMaterial);
+      ship.reactor1.scale.set(5, 5, 5);
+      ship.reactor2.scale.set(5, 5, 5);
+      ship.reactor3.scale.set(5, 5, 5);
+      scene.add(ship.reactor1);
+      scene.add(ship.reactor2);
+      scene.add(ship.reactor3);
+    });
+    scene.add(ship.light);
+    scene.ship = ship;
+  };
+  loader.load("res/joined-spaceship.glb", callback);
+};
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,74 +1,103 @@
 // ========================== functions ===========================
 
-// Return the global coordinates of an object 
-var localToGlobal = function (object) {
-    var widthHalf = window.innerWidth / 2;
-    var heightHalf = window.innerHeight / 2;
+// Return the global coordinates of an object
+var localToGlobal = function(object) {
+  var widthHalf = window.innerWidth / 2;
+  var heightHalf = window.innerHeight / 2;
 
-    var pos = object.position.clone();
-    pos.project(camera);
-    pos.x = ( pos.x * widthHalf ) + widthHalf;
-    pos.y = - ( pos.y * heightHalf ) + heightHalf;
+  var pos = object.position.clone();
+  pos.project(camera);
+  pos.x = pos.x * widthHalf + widthHalf;
+  pos.y = -(pos.y * heightHalf) + heightHalf;
 
-    return pos;
-}
+  return pos;
+};
 
 // Open source code in new tab/window
 var onClickCode = function() {
-    window.open("https://github.com/mmarie4/spacerace", '_blank').focus();
-}
+  window.open("https://github.com/mmarie4/spacerace", "_blank").focus();
+};
 
 // Pause
 var onClickPause = function() {
-    scene.pause = !scene.pause;
-    scene.pause ? document.getElementById("pause-img").setAttribute("src", "res/play-icon.png") : document.getElementById("pause-img").setAttribute("src", "res/pause-icon.png"); 
-}
+  scene.pause = !scene.pause;
+  scene.pause
+    ? document
+        .getElementById("pause-img")
+        .setAttribute("src", "res/play-icon.png")
+    : document
+        .getElementById("pause-img")
+        .setAttribute("src", "res/pause-icon.png");
+};
 
 // Load gltf for enemies
 var loadEnemyModel = function(scene) {
-    var loader = new THREE.GLTFLoader();
-    loader.load('res/joined-enemy.glb', function(gltf) {
-        scene.enemyModel = gltf.scene;
-    });
-}
+  var loader = new THREE.GLTFLoader();
+  console.log("Loader:", loader);
+  loader.load(
+    "res/joined-enemy.glb",
+    function(gltf) {
+      scene.enemyModel = gltf.scene;
+    },
+    function(xhr) {
+      console.log(
+        "Loading enemy model:",
+        (xhr.loaded / xhr.total) * 100 + "% loaded"
+      );
+    },
+    // called when loading has errors
+    function(error) {
+      console.log(error);
+    }
+  );
+};
 
 // Display/hide leaderboard
 var onClickLeaderboard = function() {
-    console.log("leaderboardDisplayed", leaderboardDisplayed); 
-    if(!leaderboardDisplayed) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", '/leaderboard', true);
-        //Send the proper header information along with the request
-        xhr.onreadystatechange = function() { // Call a function when the state changes.
-            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                var leaderboard = JSON.parse(xhr.responseText);
-                console.log("leaderboard received:", leaderboard);
-                console.log("leaderboard received:", leaderboard.length);
-                for (var i = 0; i < leaderboard.length; i++) {
-                    document.getElementById('player' + parseInt(i + 1)).innerHTML = leaderboard[i].name;
-                    document.getElementById('score' + parseInt(i + 1)).innerHTML = leaderboard[i].score;
-                }
-                document.getElementById("leaderboard-section").setAttribute("style", "display: inline-block");
-                leaderboardDisplayed = true;
-            }
+  if (!leaderboardDisplayed) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/leaderboard", true);
+    //Send the proper header information along with the request
+    xhr.onreadystatechange = function() {
+      // Call a function when the state changes.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        var leaderboard = JSON.parse(xhr.responseText);
+        for (var i = 0; i < leaderboard.length; i++) {
+          document.getElementById("player" + parseInt(i + 1)).innerHTML =
+            leaderboard[i].name;
+          document.getElementById("score" + parseInt(i + 1)).innerHTML =
+            leaderboard[i].score;
         }
-        score = {name: document.getElementById("player-text").getAttribute("value"), score: document.getElementById("time").innerHTML};
-        xhr.send();
-    } else {
-        document.getElementById("leaderboard-section").setAttribute("style", "display: none")
-        leaderboardDisplayed = false;
-    }
-}
-var onClickCross = function() {
-    document.getElementById("leaderboard-section").setAttribute("style", "display: none")
+        document
+          .getElementById("leaderboard-section")
+          .setAttribute("style", "display: inline-block");
+        leaderboardDisplayed = true;
+      }
+    };
+    score = {
+      name: document.getElementById("player-text").getAttribute("value"),
+      score: document.getElementById("time").innerHTML
+    };
+    xhr.send();
+  } else {
+    document
+      .getElementById("leaderboard-section")
+      .setAttribute("style", "display: none");
     leaderboardDisplayed = false;
-}
+  }
+};
+var onClickCross = function() {
+  document
+    .getElementById("leaderboard-section")
+    .setAttribute("style", "display: none");
+  leaderboardDisplayed = false;
+};
 
 // ========================== script execution =============================
 var leaderboardDisplayed = false;
 // Display player name
 var player = "Player" + parseInt(Math.random() * 10000);
 document.getElementById("player-text").setAttribute("value", player);
-document.getElementById('player-text').addEventListener('keyup',function(e){
-    if (e.which == 13 && this.value != "") this.blur();
+document.getElementById("player-text").addEventListener("keyup", function(e) {
+  if (e.which == 13 && this.value != "") this.blur();
 });
